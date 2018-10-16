0d0265ff681014d3dc77d23b089322c736de971e
Innovation <lt1550919167@gmail.com>
2018-10-16 17:10:52 +0800

    add ShiroConfig

---
+++ src/main/java/com/innovation/auto/config/ShiroConfig.java
@@ -0,0 +1,14 @@
+package com.innovation.auto.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther: carver
+ * @Date: 2018/10/16 15:24
+ * @email lt1550919167@163.com qq:1550919167
+ * @Description: 自定义 Realm
+ */
+@Configuration
+public class ShiroConfig {
+
+}
